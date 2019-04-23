0880c8c2238a552abac6535bfaa76dcf1d28d6ff
Tania Rascia <taniarascia@gmail.com>
2019-04-22 20:04:01 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Tic Tac Toe</title>
+    <link href="style.css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <div id="game"></div>
+  </body>
+
+  <script src="tictactoe.js"></script>
+</html>
